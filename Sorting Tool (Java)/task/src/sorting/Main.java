63c824e85db3141323080490f4e36d917f56d5fb
Naomi R. Peterson <NaomiRP@users.noreply.github.com>
2023-03-03 04:12:35 -0700

    Completed stage 1

--- Sorting Tool (Java)/task/src/sorting/Main.java
+++ Sorting Tool (Java)/task/src/sorting/Main.java
@@ -6,9 +6,25 @@ public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Long> numbers = new ArrayList<>();
+
         while (scanner.hasNextLong()) {
-            long number = scanner.nextLong();
-            // write your code here
+            numbers.add(scanner.nextLong());
         }
+
+        long max = 0;
+        int count = 0;
+
+        for (Long n : numbers) {
+            if (n > max) {
+                max = n;
+                count = 1;
+            } else if (n == max) {
+                count++;
+            }
+        }
+
+        System.out.println("Total numbers: " + numbers.size() + ".");
+        System.out.println("The greatest number: " + max + " (" + count + " time(s)).");
     }
 }
